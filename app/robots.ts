c0ddb2f4de73f06884647d53df74509a4ad36be8
luizferdazzi-commits <luiz.fer.dazzi@gmail.com>
2026-08-18 10:12:07 -0300

    Add robots.txt configuration

---
+++ app/robots.ts
@@ -0,0 +1,8 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://business-calculator-hub.vercel.app/sitemap.xml',
+  };
+}
